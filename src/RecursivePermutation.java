da50d460f96102318e27cc7695829063178015bd
Helene Rogne <helene.rogne@gmail.com>
2020-09-15 09:14:53 +0200

    Nye oppgaver

---
+++ src/RecursivePermutation.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class RecursivePermutation {
+
+    public static void main(String[] args) {
+        System.out.println("recursive permutations!" );
+
+        int values [] = {1, 2, 3};
+
+    }
+
+        public static void recursive_permutation (int [] values, int k){
+
+            if (k == values.length-1){
+                System.out.println(Arrays.toString(values));
+            }
+
+            for (int i=k; i < values.length; i ++) {
+
+            }
+    }
+    
+}
